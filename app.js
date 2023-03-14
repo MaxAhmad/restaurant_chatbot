6c94bd766201433b6398c56f35caede15b11e6fd
Maxwell Ahmadu <ahmadmax046@gmail.com>
2023-03-14 05:21:21 +0100

    socket connections

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const menu = ['vegetable salad', 'Corn', 'Agbado', 'Ewa', 'Garri', 'Casava', 'Rice', 'Beans']
+const restuarant = {
+  name: 'GoodFellas',
+  menu: ['Vegetable Salad', 'Corn', 'Ewa', 'Agbado', 'Casava', 'Garri', 'Rice'],
+  price: [2300, 800, 900, 1100, 1500, 1200, 2800]
+}
+
+const orders = [];
+const orderHistory = []
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -22,8 +29,31 @@ io.on("connection", (socket) => {
     console.log("client disconnected", socket.id);
   });
 
-  socket.emit("order", menu);
-  
+  socket.emit("order", restuarant.menu);
+
+  socket.on("order_requested", (data) => {
+    const { orderId, orderItem } = data;
+    const menuItemindex = restuarant.menu.indexOf(orderItem)
+    itemPrice = restuarant.price[menuItemindex]
+    const order = { orderId, orderItem, itemPrice};
+    orders.push(order);
+    console.log(`customer requested ${orderItem}`);
+    socket.emit("processing_order", {
+      message: `Your ${orderItem} is been processed`,
+      id: orderId,
+      order
+    });
+  });
+
+  socket.on('order_confirmed', (data) =>{
+    const {action, confirmedOrder} = data
+    orderHistory.push(confirmedOrder)
+    console.log(orderHistory)
+    if(action === 'confirmed order'){
+      socket.emit('order_history', orderHistory)
+    }
+    socket.emit('current_order', confirmedOrder)
+  })
 });
 
 server.listen(5500, () => {
